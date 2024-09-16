461bb33f54e1d78c6be07c8c50b1559a828b5a8f
rodrigocout0 <rodrigospcouto@gmail.com>
2024-09-16 20:53:20 -0300

    resolução dos exercicios

--- src/index.js
+++ src/index.js
@@ -7,13 +7,22 @@
 // area = largura * altura;
 
 // Dimensões da parede (largura e altura em metros)
+
 var largura = 5;
 var altura = 4;
 
-// Calculando a área da parede
+//Eu declaro a variável area e calculo seu valor com a fórmula abaixo.
 
-// Exibindo o resultado no console
-console.log("A área da parede é:");
+var area = largura * altura;
+
+//A variável area armazena o resultado do cálculo.
+
+
+//Depois, para imprimir na tela, eu uso + area + para concatenar a string com o valor da variável.
+//Como tenho uma string no início e outra no final, utilizo + para concatenar a variável area entre essas duas strings
+//Assim, fica + area +, onde area é a variável cujo valor será exibido.
+
+console.log("A área da parede é:" + area + " metros");
 
 //-------------------------------------------------------------------------------------------------
 
@@ -27,13 +36,13 @@ console.log("A área da parede é:");
 // Nome e sobrenome inseridos pelo usuário
 var nome = "João";
 var sobrenome = "Silva";
+// declarei a variavel pessoa = nome + sobrenome
+// usei +" "+ para criar um espaço entre o nome e o sobrenome
+var pessoa = nome +" " + sobrenome;
 
-// Juntando os nomes com a mensagem de boas-vindas
-  
-       //seu código
 
-// Exibindo a mensagem no console
-console.log();
+//Para exibir o nome completo com a mensagem de boas-vindas, usei + pessoa + para concatenar a variável pessoa com as strings da mensagem.
+console.log("Ola! " + pessoa + " Seja bem vindo!");
 
 
 //-------------------------------------------------------------------------------------------------
@@ -48,14 +57,13 @@ console.log();
 
 // Idade do funcionário em anos
 var idadeEmAnos = 30;
+// para saber a idade em dias:
+// declarei a variavel idadeDias onde ela armazena número de anos pelo número de dias por ano.
 
-// Calculando anos em dias
-  
-      //seu código
+var idadeDias = 30 * 365
 
-
-// Exibindo o resultado no console
-console.log("O funcionário viveu aproximadamente... dias");
+//para exibir o resultado eu chano a variavel idadeDias concatenada com as strings.
+console.log("O funcionário viveu aproximadamente " + idadeDias + " dias");
 
 //-------------------------------------------------------------------------------------------------
 
@@ -68,12 +76,18 @@ console.log("O funcionário viveu aproximadamente... dias");
 //  se o evento durou 135 minutos, você deve dizer que ele durou 2 horas e 15 minutos.
 
 // Duração total do evento em minutos
-var duracaoEmMinutos = 135;
+// para descobrir as horas dividi os minutos pelos minutos contidos em 1 hora
+// usei o operador % para obter o resto da divisão que são os minutos restantes
 
-// Calculando horas e minutos
-  
-      //seu código
+var duracaoEmMinutos = 135;
+var horas = Math.floor (duracaoEmMinutos / 60);
+var minutos = duracaoEmMinutos % 60;
 
 
 // Exibindo o resultado no console
-console.log("O evento durou ...horas e ... minutos.");
+console.log("O evento durou " + horas + " horas e " + minutos + " minutos.");
+
+//Fim das atividade. No desafio 4 custei a entender que ao usar Math.floor ou Math.ceil eu preciso
+//colocar parenteses para que a operação ocorra da ofrma correta sendo executado primeiro a operação e depois o arredondamento.
+// Também vacielei quando tentei concatenar as variaveis com as strings pq deixei ... e custei a descobrir que o erro era devido aos ...
+//  =)
